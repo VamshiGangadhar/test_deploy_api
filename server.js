47acc2220484295cfbb6852b08a273d9a6685270
VamshiGangadhar <vamshi.gangadhar365@gmail.com>
2023-08-22 20:50:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+// Export the Express API
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
+
+module.exports = app;
