97635e53277517036e387ba4ec02df3134537558
Hanumanthu Vamshi Gangadhar <80575671+VamshiGangadhar@users.noreply.github.com>
2023-09-04 17:05:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const os = require('os');
+const networkInterfaces = os.networkInterfaces();
+const localIPAddress = networkInterfaces['Wi-Fi'][1].address;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,8 +19,8 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/get-client-ip", (req, res) => {
-  const clientIP = req.ip; // Get the client's IP address
-  res.send(`Client IP: ${clientIP}`);
+  // const clientIP = req.ip; // Get the client's IP address
+  res.send(`Client IP: ${localIPAddress}`);
 });
 
 // Export the Express API
