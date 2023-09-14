df47d1d766058d43bd044f036aa13440f07ede20
Hanumanthu Vamshi Gangadhar <80575671+VamshiGangadhar@users.noreply.github.com>
2023-09-15 00:23:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const os = require('os');
-const networkInterfaces = os.networkInterfaces();
-const localIPAddress = networkInterfaces['Wi-Fi'][1].address;
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello from Vamshi");
@@ -17,13 +15,6 @@ app.get("/about", (req, res) => {
   res.send("https://portfolio-hvg.vercel.app/");
   // window.location.href("https://portfolio-hvg.vercel.app/");
 });
-
-app.get("/get-client-ip", (req, res) => {
-  // const clientIP = req.ip; // Get the client's IP address
-  res.send(`Client IP: ${localIPAddress}`);
-});
-
-// Export the Express API
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
